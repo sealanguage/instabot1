065c18e95eb122c6bf35ba99e38485ba7d96664a
sealanguage <elaine.froehlich@gmail.com>
2020-06-24 19:10:27 -0400

    instabot successfully logging into instagram in chrome. next step is to get past the successful login notification page

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,14 +1,25 @@
 from selenium import webdriver
+from time import sleep
+from pw import pw
+
 import os
-import time
 
-class instagramBot :
+class InstaBot :
 
-    def __init__(self, username, password) :
-        self.username = username
-        self.password = password
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome('./chromedriver')
+        self.driver.get('https://www.instagram.com/')
+        sleep(2)
 
-if __name__ == "__main__" :
-    ig_bot = instagramBot("temp_username", "temp_password")
+        self.driver.find_element_by_name('username').send_keys(username)
+        self.driver.find_element_by_name('password').send_keys(pw)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button").click()
+        sleep(4)
 
-    print(ig_bot.username)
+
+InstaBot('laneiodev', pw)
+
+# if __name__ == "__main__" :
+#     ig_bot = instagramBot("temp_username", "temp_password")
+
+     # print(ig_bot.username)
