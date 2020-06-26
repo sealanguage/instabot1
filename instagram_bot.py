0eef00562b32422dee4c0e05524f4d62491d92d6
sealanguage <elaine.froehlich@gmail.com>
2020-06-25 20:32:39 -0400

    instabot code logs into instagram and automatically goes to the user page.

--- instagram_bot.py
+++ instagram_bot.py
@@ -7,6 +7,16 @@ import os
 class InstaBot :
 
     def __init__(self, username, pw):
+        """
+            initializes an instance of the InstaBot class
+            Args:
+                username: str: the instagram user name for a user
+                pw: str: the instagram password for a user
+            Attributes:
+                driver: selenium web driver. Chrome: the chromedriver that is used to automate browser actions
+
+
+        """
         self.driver = webdriver.Chrome('./chromedriver')
         self.driver.get('https://www.instagram.com/')
         sleep(2)
@@ -14,7 +24,17 @@ class InstaBot :
         self.driver.find_element_by_name('username').send_keys(username)
         self.driver.find_element_by_name('password').send_keys(pw)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button").click()
-        sleep(4)
+        sleep(2)
+
+        # self.driver.find_element_by_name('$0').click()
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
+        sleep(2)
+
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+        sleep(2)
+
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a").click()
+        sleep(10)
 
 
 InstaBot('laneiodev', pw)
@@ -23,3 +43,6 @@ InstaBot('laneiodev', pw)
 #     ig_bot = instagramBot("temp_username", "temp_password")
 
      # print(ig_bot.username)
+
+
+
