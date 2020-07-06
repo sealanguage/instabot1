051764e415ab8bffcd1b9ad5923e015752b73188
sealanguage <elaine.froehlich@gmail.com>
2020-07-05 20:37:05 -0400

    send message if passwords do not match for instagram login bot

--- instagram_bot.py
+++ instagram_bot.py
@@ -4,6 +4,11 @@ from pw import pw
 
 import os
 
+# ask new password from the user
+new_pw = input("Enter new password : ")
+confirm_pw = input("Enter same password to confirm : ")
+
+
 class InstaBot :
 
     def __init__(self, username, pw):
@@ -21,18 +26,21 @@ class InstaBot :
         self.driver.get('https://www.instagram.com/')
         sleep(2)
 
+        # load un pw and submit login button
         self.driver.find_element_by_name('username').send_keys(username)
         self.driver.find_element_by_name('password').send_keys(pw)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button").click()
         sleep(2)
 
-        # self.driver.find_element_by_name('$0').click()
+        # click not now button
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
         sleep(2)
 
+        # click not now button notifications
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
         sleep(2)
 
+        # click to go to profile
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a").click()
         sleep(4)
 
@@ -42,11 +50,36 @@ class InstaBot :
         # self.driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a").click()
         # sleep(2)
 
+        # select the settings button
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/div[1]/div/button").click()
         sleep(2)
 
-mybot = InstaBot('laneiodev', pw)
-mybot.change_pw()
+        # select change password button
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div/button[1]").click()
+        sleep(2)
+
+        # auto fill pw into pw input
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/form/div[1]/div/input").send_keys(pw)
+        sleep(1)
+
+        # ask user for new pw
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/form/div[2]/div/input").send_keys(new_pw)
+        sleep(1)
+
+        # ask user to confirm new pw
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/form/div[3]/div/input").send_keys(confirm_pw)
+        sleep(1)
+
+        # click change password button
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/form/div[4]/div/div/button").click()
+        sleep(2)
+
+
+if(new_pw == confirm_pw) :
+    mybot = InstaBot('laneiodev', pw)
+    mybot.change_pw()
+else :
+    print("Your passwords do not match!")
 
 # if __name__ == "__main__" :
 #     ig_bot = instagramBot("temp_username", "temp_password")
